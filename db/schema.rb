8b0356dca20431a3fd1ca480ec3dba761f69d936
Jeremy Gunter <gunterja@gmail.com>
2015-12-13 16:00:30 -0600

    Add feed scaffold and set root

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151213211453) do
+ActiveRecord::Schema.define(version: 20151213215857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "entries", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "published"
-    t.text     "content"
-    t.string   "url"
-    t.string   "author"
-    t.string   "feed_"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "feeds", force: :cascade do |t|
     t.string   "name"
